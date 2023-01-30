7934d628b6afa730d7a3ba94ec003eaf2bbed588
choong <ksky9230@naver.com>
2023-01-30 19:10:26 +0900

    ThreadLocal을 사용한 쓰레드 동기화 방식

---
+++ src/test/java/hello/advanced/trace/threadlocal/code/ThreadLocalServiceTest.java
@@ -0,0 +1,50 @@
+package hello.advanced.trace.threadlocal.code;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class ThreadLocalServiceTest {
+
+    private ThreadLocalService service = new ThreadLocalService();
+
+    @Test
+    void field() {
+        log.info("main start");
+
+        //람다가 아닌 기존 방식
+       /* Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                fieldService.logic("userA");
+            }
+        };*/
+
+        Runnable userA = () -> {
+            service.logic("userA");
+        };
+        Runnable userB = () -> {
+            service.logic("userB");
+        };
+
+        Thread threadA = new Thread(userA);
+        threadA.setName("thread-A");
+        Thread threadB = new Thread(userB);
+        threadB.setName("thread-B");
+
+        threadA.start();
+        //sleep(2000); //동시성 문제 발생 X
+        sleep(100); //동시성 문제 발생 O
+        threadB.start();
+        sleep(3000);
+        log.info("main exit");
+    }
+
+    private void sleep(int millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
